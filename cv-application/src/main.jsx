82a237e1ca5fecaf37b0135fb0d9a541c11840a3
zahrabateninia <zahrabatenin@gmail.com>
2024-07-27 12:50:55 +0330

    Setup React project, display hello world.

--- cv-application/src/main.jsx
+++ cv-application/src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <h1>hello world</h1>
   </React.StrictMode>,
 )
